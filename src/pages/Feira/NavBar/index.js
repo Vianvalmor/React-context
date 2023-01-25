cee3af87f7e3f74101569d0e2767bd6fc2c6cfcd
Vinicius Andre Valmorbida <91282774+Vianvalmor@users.noreply.github.com>
2023-01-25 16:42:07 -0300

    site compra finalizado.
    carrinho saldo juros.

---
+++ src/pages/Feira/NavBar/index.js
@@ -0,0 +1,28 @@
+import { Nav } from './styles';
+import { ReactComponent as Logo } from 'assets/logo.svg';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import { useCarrinhoContext } from 'common/context/Carrinho';
+import { useHistory } from 'react-router-dom';
+
+export default function NavBar() {
+  const { quantidadeDeProdutos } = useCarrinhoContext();
+  const history = useHistory()
+  return (
+    <Nav>
+      <Logo />
+      <IconButton
+        disabled={quantidadeDeProdutos === 0}
+        onClick={() => history.push('/carrinho')}
+      >
+        <Badge
+          color="primary"
+          badgeContent={quantidadeDeProdutos}
+        >
+          <ShoppingCartIcon />
+        </Badge>
+      </IconButton>
+    </Nav>
+  )
+}
